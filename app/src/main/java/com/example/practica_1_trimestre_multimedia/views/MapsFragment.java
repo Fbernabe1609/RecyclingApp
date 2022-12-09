d788b9faf4b587e5f2254f4cc9565bf7e233b68a
Fbernabe1609 <fbernabe160903@gmail.com>
2022-12-09 15:16:13 +0100

    Avance clase

--- app/src/main/java/com/example/practica_1_trimestre_multimedia/views/MapsFragment.java
+++ app/src/main/java/com/example/practica_1_trimestre_multimedia/views/MapsFragment.java
@@ -12,7 +12,6 @@ import android.view.ViewGroup;
 import com.example.practica_1_trimestre_multimedia.R;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -20,23 +19,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsFragment extends Fragment {
 
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
-
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        }
+    private OnMapReadyCallback callback = googleMap -> {
+        LatLng granada = new LatLng(37.18817, -3.60667);
+        googleMap.addMarker(new MarkerOptions().position(granada).title("Marker in Granada"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(granada));
     };
 
     @Nullable
